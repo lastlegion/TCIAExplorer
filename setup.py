b5e8726e23419d9ae55553654d9dcb4fbdbfa857
Ganesh Iyer <lastlegion@gmail.com>
2015-07-10 15:00:05 +0530

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='tciaexplorer',
         version='0.1',
-        packages=['tciaexplorer'],
+        packages=['tciaexplorer', 'tciaexplorer.TciaExplorer'],
         author='Ganesh Iyer',
         install_requires=[
             'setuptools',
